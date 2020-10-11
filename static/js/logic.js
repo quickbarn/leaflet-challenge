7006a00d08a76d4b5fa417e0576affe5f8b527f1
quickbarn <barnabas.lee@gmail.com>
2020-10-11 16:48:12 -0700

    stuck, starting over. moved back to variable defining

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,47 +1,13 @@
-
-
-
-
 // mapbox accessToken:apiKey
+
 //tile layers variables of map(s)
+
 var mapGrayScale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    accessToken:apiKey
+
+  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+
+  maxZoom: 18,
+
+  accessToken:apiKey
+
 });
-
-var mapSatellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    accessToken:apiKey
-});
-
-var mapOutdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    accessToken:apiKey
-});
-
-var map = L.map("#map", {
-    center: [38, -96],
-    zoom: 6,
-    layers: [mapGrayScale, mapSatellite, mapOutdoors]
-  });
-
-//data objects for map layer
-mapGrayScale.addTo(map);
-
-
-
-//layer(s) to map
-var mapLayers = {
-    Satellite: mapSatellite,
-    Grayscale: mapGrayScale,
-    Outdoors: mapOutdoors
-  };
-
-//desired datasets
-var earthquakeData = new L.LayerGroup();
-
-var dataOnMaps = {
-    "Earthquakes": earthquakeData
-};
-
-L
-  .control
-  .layers(mapLayers, dataOnMaps)
-  .addTo(map);
