d8b3adc5ce24c093b03412ef3d10e70f84d82fb5
quickbarn <barnabas.lee@gmail.com>
2020-10-11 18:28:06 -0700

    identified error in search for data within json, also missing ending brackets to run code

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 //tile layers variables of map(s)
 var mapGrayScale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 18,
+  maxZoom: 20,
   accessToken:apiKey
 });
 
@@ -16,58 +16,52 @@ var map =L.map("map", {
 mapGrayScale.addTo(map);
 
 //layer for earthquake data from USGS
-var earthquakeData = new L.layerGroup();
+var earthquakeData = new L.LayerGroup();
 
- // d3 pull for geoJson from USGS
- d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
-     function styleFxn(feature) {
+// d3 pull for geoJson from USGS
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
+    function styleFxn(feature) {
          return {
-             opacity:.75,
-             fillOpacity:1,
-             fillColor: colorFxn(features.properties.mag),
-             color: "#000000",
-             radius: radiusFxn(features.properties.mag),
+             opacity: 0.75,
+             fillOpacity: 1,
+             fillColor: colorFxn(feature.properties.mag),
+             radius: radiusFxn(feature.properties.mag),
              stroke:true, 
              weight: 1
          };
-     };
-// Color function for earthquake scale, color palette from color-hex.com/color-palette/27541
-
-function colorFxn(mag) {
-    switch (true){
-        case mag > 5:
-            return "#ee3e32";
-    ​    case mag > 4:
-            return "#f68838";
-    ​    case mag > 3:
-            return "#fbb021";
-    ​    case mag > 2:
-            return "#1b8a5a";
-    ​    case mag > 1:
-            return "#1d4877";
-    ​    default:
-            return "#1d4877";
-   };
-};
-
-  // define the radius of the earthquake marker based on its magnitude.
-  function radiusFxn(mag) {
-    if (mag === 0) {
-      return 1;
     }
 
-    return mag * 2;
-  }
-
-//geoJson to map layers
-L.geoJson(data, {
-    pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-    },
-    style: styleFxn,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("Mag: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    // Color function for earthquake scale, color palette from color-hex.com/color-palette/27541 
+    function colorFxn(mag) {
+        switch (true){
+            case mag > 5: return "#ee3e32";
+            case mag > 4: return "#f68838";
+            case mag > 3: return "#fbb021";
+            case mag > 2: return "#1b8a5a";
+            case mag > 1: return "#1d4877";
+            default: return "#1d4878";
+        }
     }
-}).addTo(earthquakeData);
 
-earthquakeData.addTo(map);
+    // radius sized from magnitude scale
+    function radiusFxn(mag) {
+        if (mag == 0) {
+            return 1;
+        }
+        return mag * 2;
+    }
+
+    //geoJson to map layers
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleFxn,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("mag: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(earthquakeData);
+
+    earthquakeData.addTo(map);
+    
+});
